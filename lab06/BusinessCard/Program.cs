0895d9bc6e93931a71098367ba1d47ede7447e2a
lmProgramming <mikikubs@gmail.com>
2024-11-18 16:20:41 +0100

    feat: created a zipper helper script and initialized lab06

---
+++ lab06/BusinessCard/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BusinessCard
+{
+    class Program
+    {
+        static void Main()
+        {
+            var person = ("Jan", "Kowalski", 30, 5000.0);
+            PrintTuple(person);
+        }
+
+        static void PrintTuple((string FirstName, string LastName, int Age, double Salary) person)
+        {
+            Console.WriteLine($"Imię: {person.FirstName}, Nazwisko: {person.LastName}, Wiek: {person.Age}, Płaca: {person.Salary}");
+
+            var (firstName, lastName, age, salary) = person;
+            Console.WriteLine($"Imię: {firstName}, Nazwisko: {lastName}, Wiek: {age}, Płaca: {salary}");
+
+            Console.WriteLine($"Imię: {person.Item1}, Nazwisko: {person.Item2}, Wiek: {person.Item3}, Płaca: {person.Item4}");
+        }
+    }
+}
