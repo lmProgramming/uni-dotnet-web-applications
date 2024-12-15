954502ddbcb701689eb53c5d9166a16ee84d0424
lmProgramming <mikikubs@gmail.com>
2024-12-15 18:45:33 +0100

    feat: fixed zipper to exclude any directories by professor and build directories

--- zipper.py
+++ zipper.py
@@ -1,18 +1,27 @@
 import os
 import zipfile
+import re
 
 
-def zip_files(folder_path, output_zip):
+def zip_files(folder_path, output_zip) -> None:
     with zipfile.ZipFile(output_zip, 'w') as zipf:
-        for root, dirs, files in os.walk(folder_path):
-            x = input()
-            if x:
+        for root, _, files in os.walk(folder_path):
+            if root.startswith(f"{folder_path}\Web"):
+                continue
+            # match folder_path\xyz\bin or folder_path\xyz\obj or folder_path\xyz\.vs
+            match: re.Match[str] | None = re.match(
+                rf"{folder_path}\\[^\\]+\\(.+)", root)
+            if match and any(str(match.group(1)).startswith(beginning) for beginning in ["bin", "obj", ".vs"]):
                 continue
             for file in files:
                 if not file.endswith('.pdf'):
                     file_path = os.path.join(root, file)
                     zipf.write(file_path, os.path.relpath(
                         file_path, folder_path))
+
+
+def check(text: str, *checked_texts) -> bool:
+    return any([end in text for end in checked_texts])
 
 
 if __name__ == "__main__":
