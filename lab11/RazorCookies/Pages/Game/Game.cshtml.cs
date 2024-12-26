48686a9a54d088cda7bca29de8a1e4c8ece89d81
lmProgramming <mikikubs@gmail.com>
2024-12-26 18:58:54 +0100

    refactor: refactor number guessing game structure and update routing

--- lab11/RazorCookies/Pages/Game/Game.cshtml.cs
+++ lab11/RazorCookies/Pages/Game/Game.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Drawing;
 
-namespace RazorCookies.Pages
+namespace RazorCookies.Pages.Game
 {
     public class GameModel : PageModel
     {
