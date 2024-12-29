16e49e97a471b01a24f2775811437e50daa04d62
jantar <mikikubs@gmail.com>
2024-12-29 13:33:09 +0100

    feat: added CRUD for categories

---
+++ lab11/RazorCookies/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EntityFramework.Data;
+using EntityFramework.Models;
+
+namespace RazorCookies.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ArticleDbContext _context;
+
+        public IndexModel(ArticleDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Categories.ToListAsync();
+        }
+    }
+}
