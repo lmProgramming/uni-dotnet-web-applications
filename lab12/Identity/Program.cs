bb8c32b72354378006a77ed040ec889e05aab8b4
jantar <mikikubs@gmail.com>
2025-01-15 15:46:40 +0100

    feat: admin works correctly now

--- lab12/Identity/Program.cs
+++ lab12/Identity/Program.cs
@@ -12,7 +12,8 @@ internal class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+        builder.Services.AddSession();
+
         builder.Services.AddControllersWithViews()
             .AddNewtonsoftJson(options =>
             {
@@ -38,13 +39,13 @@ internal class Program
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
+
+        app.UseSession();
 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
